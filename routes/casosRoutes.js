89ba4a81901ef67cbcbf3e2c427b1c92c1fe37b8
Gabriel3637 <gabriellvbs14@gmail.com>
2025-07-23 11:53:49 -0300

    Corrigido os erros de nomenclatura dos arquivos, filtros de casos, validacao do uuid, resposta do delete e middleware

--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -3,11 +3,11 @@ const routerCaso = express.Router();
 const casosController = require('../controllers/casosController');
 
 // define a rota para /agentes usando o método GET
-routerCaso.get('/casos', casosController.getAllCasos);
-routerCaso.get('/casos/:id', casosController.getCaso);
-routerCaso.post('/casos', casosController.postCaso);
-routerCaso.put('/casos/:id', casosController.putCaso);
-routerCaso.patch('/casos/:id', casosController.patchCaso);
-routerCaso.delete('/casos/:id', casosController.deleteCaso);
+routerCaso.get('/', casosController.getAllCasos);
+routerCaso.get('/:id', casosController.getCaso);
+routerCaso.post('/', casosController.postCaso);
+routerCaso.put('/:id', casosController.putCaso);
+routerCaso.patch('/:id', casosController.patchCaso);
+routerCaso.delete('/:id', casosController.deleteCaso);
 
 module.exports = routerCaso
